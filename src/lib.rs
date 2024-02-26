501a63ff35fe92905294973b0afc92ad718e93d0
guiburon <guillaume.buron@yahoo.fr>
2024-02-26 17:59:24 +0100

    PoC dense mat-mat product

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
+use faer::modules::core::mul::matmul;
+use faer::{mat, Mat, Parallelism};
 use std::usize;
+
+use faer::{
+    modules::core::reborrow::{Reborrow, ReborrowMut},
+    MatMut, MatRef,
+};
 
 #[no_mangle]
 pub unsafe extern "C" fn make_zero(
@@ -11,7 +18,7 @@ pub unsafe extern "C" fn make_zero(
     assert!(!a_ptr.is_null());
 
     let mut a = unsafe {
-        faer::mat::from_raw_parts_mut::<f64>(
+        mat::from_raw_parts_mut::<f64>(
             a_ptr,
             nrows as usize,
             ncols as usize,
@@ -22,3 +29,105 @@ pub unsafe extern "C" fn make_zero(
 
     a.fill_zero();
 }
+
+#[no_mangle]
+pub unsafe extern "C" fn mult(
+    c_ptr: *mut f64,
+    c_nrows: u64,
+    c_ncols: u64,
+    c_row_stride: u64,
+    c_col_stride: u64,
+    a_ptr: *const f64,
+    a_nrows: u64,
+    a_ncols: u64,
+    a_row_stride: u64,
+    a_col_stride: u64,
+    b_ptr: *const f64,
+    b_nrows: u64,
+    b_ncols: u64,
+    b_row_stride: u64,
+    b_col_stride: u64,
+) {
+    assert!(!c_ptr.is_null());
+    assert!(!a_ptr.is_null());
+    assert!(!b_ptr.is_null());
+
+    let c = unsafe {
+        mat::from_raw_parts_mut::<f64>(
+            c_ptr,
+            c_nrows as usize,
+            c_ncols as usize,
+            c_row_stride as isize,
+            c_col_stride as isize,
+        )
+    };
+
+    let a = unsafe {
+        mat::from_raw_parts::<f64>(
+            a_ptr,
+            a_nrows as usize,
+            a_ncols as usize,
+            a_row_stride as isize,
+            a_col_stride as isize,
+        )
+    };
+
+    let b = unsafe {
+        mat::from_raw_parts::<f64>(
+            b_ptr,
+            b_nrows as usize,
+            b_ncols as usize,
+            b_row_stride as isize,
+            b_col_stride as isize,
+        )
+    };
+
+    matmul(c, a, b, None, 1.0, Parallelism::None);
+}
+
+// #[no_mangle]
+// pub unsafe extern "C" fn add(
+//     c_ptr: *mut f64,
+//     a_ptr: *const f64,
+//     b_ptr: *const f64,
+//     nrows: u64,
+//     ncols: u64,
+//     row_stride: u64,
+//     col_stride: u64,
+// ) {
+//     assert!(!c_ptr.is_null());
+//     assert!(!a_ptr.is_null());
+//     assert!(!b_ptr.is_null());
+//
+//     let mut c = unsafe {
+//         faer::mat::from_raw_parts_mut::<f64>(
+//             c_ptr,
+//             nrows as usize,
+//             ncols as usize,
+//             row_stride as isize,
+//             col_stride as isize,
+//         )
+//     };
+//
+//     let a = unsafe {
+//         faer::mat::from_raw_parts::<f64>(
+//             a_ptr,
+//             nrows as usize,
+//             ncols as usize,
+//             row_stride as isize,
+//             col_stride as isize,
+//         )
+//     };
+//
+//     let b = unsafe {
+//         faer::mat::from_raw_parts::<f64>(
+//             b_ptr,
+//             nrows as usize,
+//             ncols as usize,
+//             row_stride as isize,
+//             col_stride as isize,
+//         )
+//     };
+//     faer::zipped!(&mut c.rb_mut(), a.rb(), b.rb())
+//         .for_each(|faer::unzipped!(c, a, b)| *c = *a + *b);
+// }
--- wrapper.jl
+++ wrapper.jl
@@ -1,16 +1,62 @@
 module faer
 
 export make_zero!
+export mult!
 
 using Libdl
 using LinearAlgebra
 
 lib = dlopen("target/debug/libfaer_api.so")
-fun = dlsym(lib, "make_zero")
+fun_make_zero = dlsym(lib, "make_zero")
+fun_mult = dlsym(lib, "mult")
 
 function make_zero!(a::StridedMatrix{Float64})
-  ccall(fun, Cvoid, (Ptr{Cdouble}, Culonglong, Culonglong, Culonglong, Culonglong), a, size(a)[1], size(a)[2], strides(a)[1], strides(a)[2])
+  ccall(fun_make_zero, Cvoid, (Ptr{Cdouble}, Culonglong, Culonglong, Culonglong, Culonglong), a, size(a)[1], size(a)[2], strides(a)[1], strides(a)[2])
   return a
+end
+
+function mult!(c::T, a::T, b::T) where {T<:StridedMatrix{<:Float64}}
+  (size(a)[2] == size(b)[1]) || throw(DimensionMismatch("a versus b"))
+  (size(c) == (size(a)[1], size(b)[2])) || throw(DimensionMismatch("c versus a and b"))
+
+  ccall(
+    fun_mult,
+    Cvoid,
+    (
+      Ptr{Cdouble},
+      Culonglong,
+      Culonglong,
+      Culonglong,
+      Culonglong,
+      Ptr{Cdouble},
+      Culonglong,
+      Culonglong,
+      Culonglong,
+      Culonglong,
+      Ptr{Cdouble},
+      Culonglong,
+      Culonglong,
+      Culonglong,
+      Culonglong,
+    ),
+    c,
+    size(c)[1],
+    size(c)[2],
+    strides(c)[1],
+    strides(c)[2],
+    a,
+    size(a)[1],
+    size(a)[2],
+    strides(a)[1],
+    strides(a)[2],
+    b,
+    size(b)[1],
+    size(b)[2],
+    strides(b)[1],
+    strides(b)[2]
+  )
+
+  return c
 end
 
 end # module
